cdc1278aa7d114b7188c852437c1fc8d7f98746a
dvinferno <72324421+dvinferno@users.noreply.github.com>
2023-06-02 16:08:34 -0400

    added OrderDatastore listAsync method

--- example/example.dart
+++ example/example.dart
@@ -4,6 +4,6 @@ void main(List<String> arguments) async {
   const String apiKey = "hSwisUHs9EOotFlV94cFtnd7aN7Quy/0u9ZRw9q0SqC/bUzL";
   var uni = Universe(universeId: 3294070703, apiKey: apiKey);
   var datastore = OrderedDatastore(universe: uni, orderedDatastoreName: 'TopPlaytime');
-  var info = await datastore.listAsync();
+  var info = await datastore.listAsync(maxPageSize: 2, orderBy: 'desc');
   print(info);
 }
--- lib/src/ordered_datastore.dart
+++ lib/src/ordered_datastore.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
-import 'package:opencloud_api_wrapper/src/datastore.dart';
 import 'http_request_exception.dart';
 import 'universe.dart';
 import 'urls.dart';
@@ -22,12 +21,30 @@ class OrderedDatastore {
   /// [universe] and [orderedDatastoreName] are required. [scope] defaults to "global".
   OrderedDatastore({required this.universe, required this.orderedDatastoreName, this.scope = "global"});
 
-  Future<String> listAsync({int? maxPageSize = 10, String? pageToken, String? orderBy}) async {
+  Future<Map<String, dynamic>> listAsync({int? maxPageSize = 10, String? pageToken, String? orderBy}) async {
     int universeId = universe.universeId;
     String apiKey = universe.apiKey;
 
-    String baseUrl = '$ORDERED_DATASTORE_API_BASE_ENDPOINT';
+    // Construct the base URL with maxPageSize parameter
+    String baseUrl =
+        '$ORDERED_DATASTORE_API_BASE_ENDPOINT/$universeId/orderedDataStores/$orderedDatastoreName/scopes/$scope/entries?max_page_size=$maxPageSize';
 
-    return baseUrl;
+    // Add pageToken and orderBy parameters if provided
+    if (pageToken != null) baseUrl += '&page_token=$pageToken';
+    if (orderBy != null) baseUrl += '&order_by=$orderBy';
+
+    // Parse the URL and send a GET request with the API key header
+    final endpoint = Uri.parse(baseUrl);
+    final response = await http.get(endpoint, headers: {'x-api-key': apiKey});
+
+    // Throw an exception if the response status code is not 200
+    if (response.statusCode != 200) {
+      throw HttpRequestException(
+          universeId: universeId, statusCode: response.statusCode, reasonPhrase: response.reasonPhrase);
+    }
+
+    // Decode the response body and create a DatastoreList object from it
+    final responseJson = json.decode(response.body) as Map<String, dynamic>;
+    return responseJson;
   }
 }
